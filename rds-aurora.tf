c5e64c35cabf1664d6770e4184ee5f92f6173638
EC2 Default User <ec2-user@ip-10-0-1-196.ap-northeast-2.compute.internal>
2020-09-08 06:07:54 +0000

    rds-aurora

--- rds-aurora.tf
+++ rds-aurora.tf
@@ -6,6 +6,7 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
   instance_class = "db.t2.small"
   engine = aws_rds_cluster.default.engine
   engine_version = aws_rds_cluster.default.engine_version
+  db_parameter_group_name = "terraform-db-pg"
 }
 
 resource "aws_rds_cluster" "default" {
@@ -19,7 +20,7 @@ resource "aws_rds_cluster" "default" {
   engine_version = "5.7.mysql_aurora.2.03.2"
   db_subnet_group_name = "terraform_db_subnet_group"
   vpc_security_group_ids = [aws_security_group.terraform-db-sg.id]
-  #db_cluster_parameter_group_name = "terraform-db-cluster-pg"
+  db_cluster_parameter_group_name = "terraform-db-cluster-pg"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
 }
