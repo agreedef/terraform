bcf1d6067ce662bdad76037527451c6e9f2a9923
EC2 Default User <ec2-user@ip-10-0-1-196.ap-northeast-2.compute.internal>
2020-09-08 05:34:55 +0000

    rds-aurora

--- rds-aurora.tf
+++ rds-aurora.tf
@@ -1,10 +1,11 @@
 resource "aws_rds_cluster_instance" "cluster_instances" {
 
-  identifier = "aurora-mysql-instance"
+  count = 2
+  identifier = "aurora-mysql-instance-${count.index}"
   cluster_identifier = aws_rds_cluster.default.id
   instance_class = "db.t2.small"
-  engine = "aurora-mysql"
-  engine_version = "5.7.mysql_aurora.2.03.2"
+  engine = aws_rds_cluster.default.engine
+  engine_version = aws_rds_cluster.default.engine_version
 }
 
 resource "aws_rds_cluster" "default" {
@@ -14,6 +15,8 @@ resource "aws_rds_cluster" "default" {
   database_name = "terraform"
   master_username = "admin"
   master_password = "123123123"
+  engine = "aurora-mysql"
+  engine_version = "5.7.mysql_aurora.2.03.2"
   db_subnet_group_name = "terraform_db_subnet_group"
   vpc_security_group_ids = [aws_security_group.terraform-db-sg.id]
   #db_cluster_parameter_group_name = "terraform-db-cluster-pg"
